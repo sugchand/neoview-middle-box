10af498c459fc2e2ff8c897023abe44ba889153c
Sugesh Chandran <sugesh.chandran@gmail.com>
2016-07-11 18:49:41 +0100

    Adding exception file for custom exceptions.
    Signed-off-by: Sugesh Chandran <sugesh.chandran@gmail.com>

---
+++ src/nv_exception.py
@@ -0,0 +1,8 @@
+#! /usr/bin/python3
+# -*- coding: utf8 -*-
+# All the exception definition for the nv-middlebox goes here.
+#
+__author__ = "Sugesh Chandran"
+__copyright__ = "Copyright (C) The neoview team."
+__license__ = "GNU Lesser General Public License"
+__version__ = "1.0
