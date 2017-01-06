3eae39060543f36e9955e34669431868c773376e
Nikhil T Nair <nikhil.nair@plancess.com>
2017-01-06 14:48:31 +0530

    remove bower and add as libraries

--- src/nv_midbox_websock/templates/static/js/app.js
+++ src/nv_midbox_websock/templates/static/js/app.js
@@ -1,43 +1,14 @@
 angular.module('landingApp', [])
-.controller('landingController', ['$scope', function($scope) {
-
-  ws = new WebSocket("ws://localhost:8080/userwebsocket");
-  
+.controller('landingController', ['$scope', '$window', function($scope, $window) {
+  var host = "ws://" + $window.location.host + "/userwebsocket"
+  ws = new WebSocket(host);
   ws.onmessage = function(e) {
-    console.log(e.data);
-  };     
+    $scope.cameraInfo = JSON.parse(e.data);
+    $scope.$apply();
+  };
 
-  // $scope.cameraInfo = [
-  //   {
-  //     id : 1,
-  //     name: "Camera1",
-  //     status: 0,
-  //     description: "Baby room camera"
-  //   },{
-  //     id : 2,
-  //     name: "Camera2",
-  //     status: 1,
-  //     description: "ICU room camera"
-  //   },{
-  //     id : 3,
-  //     name: "Camera3",
-  //     status: 1,
-  //     description: "Hallway camera"
-  //   },{
-  //     id : 4,
-  //     name: "Camera4",
-  //     status: 0,
-  //     description: "Incubator camera"
-  //   },{
-  //     id : 5,
-  //     name: "Camera5",
-  //     status: 1,
-  //     description: "Bedside camera"
-  //   },{
-  //     id : 6,
-  //     name: "Camera6",
-  //     status: 1,
-  //     description: "Test one"
-  //   }
-  // ];
+  $scope.changeChk = function(cameraInfo, status) {
+    cameraInfo.status = status;
+    ws.send(JSON.stringify(cameraInfo));
+  };
 }]);
