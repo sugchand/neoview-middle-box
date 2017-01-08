1fc616701327eb903543df9a3d91b45433106f2a
Nikhil T Nair <nikhiltnair@outlook.com>
2017-01-09 01:56:57 +0530

    disable the camera button in off case (#4)
    Disable the button on a user OFF input.
    
    Fix the bug on web-switch. web-switch was showing a camera even there is no camera present on middlebox.

--- src/nv_midbox_websock/templates/static/js/app.js
+++ src/nv_midbox_websock/templates/static/js/app.js
@@ -1,14 +1,38 @@
+'use strict'
 angular.module('landingApp', [])
 .controller('landingController', ['$scope', '$window', function($scope, $window) {
-  var host = "ws://" + $window.location.host + "/userwebsocket"
+  var host = "ws://" + $window.location.host + "/userwebsocket",
   ws = new WebSocket(host);
   ws.onmessage = function(e) {
-    $scope.cameraInfo = JSON.parse(e.data);
+    var parsed = JSON.parse(e.data);
+    $scope.cameraInfo = [];
+    for(var i=0;i<parsed.length;i++) {
+      if(!isEmpty(parsed[i])) {
+        $scope.cameraInfo[i] = parsed[i];
+      }
+    }
     $scope.$apply();
   };
 
-  $scope.changeChk = function(cameraInfo, status) {
+  $scope.changeChk = function(index, status) {
+    var cameraInfo = $scope.cameraInfo[index];
+    delete cameraInfo.disabled;
     cameraInfo.status = status;
     ws.send(JSON.stringify([cameraInfo]));
+    if(status === 1) {
+      $scope.cameraInfo[index].disabled = true;
+    } else {
+      $scope.cameraInfo[index].disabled = false;
+    }
   };
+
+  function isEmpty(obj) {
+    for (var prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        return false;
+      }
+    }
+    return true;
+  };
+
 }]);
