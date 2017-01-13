b0fb597906ef5bc09b7bf85e2f92772f52445239
Nikhil T Nair <nikhil.nair@plancess.com>
2017-01-13 12:15:21 +0530

    resolve the cache issue and on/off issue

--- src/nv_midbox_websock/templates/static/js/app.js
+++ src/nv_midbox_websock/templates/static/js/app.js
@@ -2,6 +2,7 @@
 angular.module('landingApp', [])
 .controller('landingController', ['$scope', '$window', function($scope, $window) {
   var host = "ws://" + $window.location.host + "/userwebsocket",
+  cameraStatus = [],
   ws = new WebSocket(host);
   ws.onmessage = function(e) {
     var parsed = JSON.parse(e.data);
@@ -10,21 +11,32 @@ angular.module('landingApp', [])
     for(var i=0;i<parsed.length;i++) {
       if(!isEmpty(parsed[i])) {
         $scope.cameraInfo[i] = parsed[i];
+        if(cameraStatus.length > 0) {
+          _.each(cameraStatus, function(cameraSt, index) {
+            if(i === index && $scope.cameraInfo[i].status === 3) {
+              $scope.cameraInfo[index].disabled = true;
+              $scope.cameraInfo[i].status = cameraSt == 1 ? 2 : 1;
+            }
+          })
+        }
       }
     }
     $scope.$apply();
   };
 
-  $scope.changeChk = function(index, status, disableFlg) {
+  $scope.changeChk = function(index, new_status, disableFlg) {
     if(disableFlg) {
+      $scope.cameraInfo[index].disabled = true;
+      $scope.cameraInfo[index].status = new_status == 1 ? 2 : 1;
       alert("Camera is busy, Please wait....");
     } else {
+      cameraStatus[index] = new_status;
       var cameraInfo = $scope.cameraInfo[index];
       delete cameraInfo.disabled;
-      cameraInfo.status = status;
+      cameraInfo.status = new_status;
       ws.send(JSON.stringify([cameraInfo]));
       $scope.cameraInfo[index].disabled = true;
-      $scope.cameraInfo[index].status = status == 1 ? 2 : 1;
+      $scope.cameraInfo[index].status = new_status == 1 ? 2 : 1;
     }
   };
 
